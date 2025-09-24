afa72f443d3e42f10c884a8fb252556a28b61074
shahnawaz123abl <training@thinnkware.com>
2025-09-24 17:21:08 +0530

    Added some features on admin dashboard

--- src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/pages/AdminDashboard/AdminDashboard.jsx
@@ -61,6 +61,23 @@ const AdminDashboard = () => {
   }, [activeContent]);
 
   useEffect(() => {
+  const html = document.documentElement;
+  const body = document.body;
+  if (isSidebarOpen) {
+    html.style.overflow = 'hidden';
+    body.style.overflow = 'hidden';
+  } else {
+    html.style.overflow = '';
+    body.style.overflow = '';
+  }
+  return () => {
+    html.style.overflow = '';
+    body.style.overflow = '';
+  };
+}, [isSidebarOpen]);
+
+
+  useEffect(() => {
     if (isAuthenticated !== 'true') {
       navigate('/login');
       return;
